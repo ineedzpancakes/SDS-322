0b7b8f4cf7642b699eb9fdaddf4fdf9d55daa1a0
Carlos Miranda <ineedzpancakes@gmail.com>
2021-05-07 21:09:43 -0500

    Update intro.cpp

--- intro.cpp
+++ intro.cpp
@@ -5,5 +5,5 @@ using std::endl;
 
 int main()
 {
-cout << "Hello my name is Carlos Miranda, and I am " << 2020 - 2001 << endl;
+cout << "Hello my name is Carlos, and I am " << 2020 - 2001 << endl;
 }
